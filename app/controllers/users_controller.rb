64e58ad446d82b9a219d555966cfe26ec0e325c1
Ryan Havens <ryan.d.havens@gmail.com>
2015-11-15 17:28:09 +0100

    Added admin functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,17 @@ class UsersController < AuthController
 
 	def show
 		@user = User.find(params[:id])
+		@users = []
+		if (current_user.uid == "473418502829853")
+			@users = User.all
+		end
+	end
+
+	def destroy
+		if (current_user.uid == "473418502829853")
+			User.find(params[:id]).destroy
+			flash[:success] = "User destroyed"
+			redirect_to :home
+		end
 	end
 end
