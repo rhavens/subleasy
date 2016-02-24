38b2302b193f1b0f870e3c813ac70b0941e8188c
Ryan Havens <ryan.d.havens@gmail.com>
2016-02-24 17:02:02 -0500

    Removed auth requirement from offers page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,4 @@
-class OffersController < ApplicationController
+class OffersController < AuthController
 	before_action :correct_user, only: [:edit, :update, :destroy]
 	before_action :set_offer, only: [:show, :edit, :update, :destroy]
 	skip_before_action :authenticate_user!, only: [:index, :show]
